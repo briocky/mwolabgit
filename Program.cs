b761eb8d9a43e1ae1c62c3fa2a9e7907972409fe
dobos <doboszdav@gmail.com>
2023-12-01 13:00:06 +0100

    changed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "!Hello World!!");
+var version = typeof(Program).Assembly.GetName().Version;
+var formattedVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+app.MapGet("/", () => $"Hello World! Version: {formattedVersion}");
 
 app.Run();
